dcfc9d1fcb749d6cbdb6e6e90cf1618e85bc388e
geekymon2 <geekymon2@gmail.com>
2026-04-07 19:45:39 +1000

    Added Sidebar view

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { TreeDataProvider } from './TreeeDataProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	 vscode.window.showInformationMessage("Extension started!");
+	vscode.window.showInformationMessage("Gops extension started!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	vscode.window.createTreeView("myView", {
+    treeDataProvider: new TreeDataProvider(),
+	});
+	
 	console.log('Congratulations, your extension "gops" is now active!');
 
 	// The command has been defined in the package.json file
