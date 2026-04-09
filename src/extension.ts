10ac733ad7279a3cc9afc97478498897ab4b4b69
geekymon2 <geekymon2@gmail.com>
2026-04-09 19:38:44 +1000

    updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { TreeDataProvider } from './TreeeDataProvider';
+import { TreeDataProvider } from './TreeDataProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	vscode.window.showInformationMessage("Gops extension started!");
 
-	vscode.window.createTreeView("myView", {
-    treeDataProvider: new TreeDataProvider(),
-	});
+  const treeDataProvider = new TreeDataProvider();
+  vscode.window.registerTreeDataProvider("gitOpsView", treeDataProvider);
 	
 	console.log('Congratulations, your extension "gops" is now active!');
 
