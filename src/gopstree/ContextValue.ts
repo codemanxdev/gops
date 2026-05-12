c7973b24eefbacc2ac1b3c9db19df2ab4843a349
geekymon2 <geekymon2@gmail.com>
2026-05-12 17:34:25 +1000

    updated

--- src/gopstree/ContextValue.ts
+++ src/gopstree/ContextValue.ts
@@ -2,6 +2,7 @@ export enum ContextValue {
   Repository = "repository",
   Changes = "changes",
   LocalBranches = "localBranches",
+  LocalBranchesCurrent = "localBranches.current",
   RemoteBranches = "remoteBranches",
   Branch = "branch",
   File = "file",
--- src/gopstree/context/Contexts.ts
+++
@@ -1,3 +0,0 @@
-export const CONTEXTS = {
-  IS_CURRENT_BRANCH: "gops.isCurrentBranch",
-} as const;
